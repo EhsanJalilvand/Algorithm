1909ea0d889d95fdd82cb0cf83680b0689cc50cb
e.jalilvand <e.jalilvand@JALILVAND-08>
2025-02-18 15:14:03 +0330

    recursive added

---
+++ QuickSort/Recursive.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class Recursive
+    {
+        int fact(int n)
+        {
+            if (n <= 1) return 1;
+            return n * fact(n - 1);
+
+        }
+        int fib(int n)
+        {
+            if (n <= 1) return 1;
+            return fib(n - 1) + fib(n - 2);
+
+        }
+    }
+}
