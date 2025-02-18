efaa5cc6ad6eb6b17f9870047dc54e8ba3eb04f5
e.jalilvand <e.jalilvand@JALILVAND-08>
2025-02-18 15:59:08 +0330

    bug fixes

--- QuickSort/QuickSort.cs
+++ QuickSort/QuickSort.cs
@@ -5,35 +5,57 @@ namespace QuickSort
 	{
 		public static int[] QuickSort(this int[] array)
 		{
-            return SortArray(array, 0, array.Length - 1);
+            return Sort(array, 0, array.Length - 1);
 		}
-        static int[]  SortArray(int[] array, int leftindex, int rightIndex)
+        public static int[] Sort(int[] arr, int low, int high)
         {
-            int i = leftindex;
-            int j = rightIndex;
-            int pivot = array[leftindex];
+            if (low < high)
+            {
+                // پیدا کردن اندیس pivot
+                int pivotIndex = Partition(arr, low, high);
+
+                // مرتب‌سازی بازه‌های چپ و راست pivot
+                Sort(arr, low, pivotIndex - 1);  // بازه چپ
+                Sort(arr, pivotIndex + 1, high); // بازه راست
+            }
+            return arr;
+        }
+
+        // تابع Partition برای تقسیم آرایه بر اساس pivot
+        private static int Partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[low]; // انتخاب اولین عنصر به عنوان pivot
+            int i = low + 1;      // شروع از عنصر بعدی pivot
+            int j = high;
+
             while (i <= j)
             {
-                while (array[i] < pivot)
+                // پیدا کردن عنصری بزرگتر از pivot
+                while (i <= j && arr[i] <= pivot)
                     i++;
-                while (array[j] > pivot)
+
+                // پیدا کردن عنصری کوچکتر از pivot
+                while (i <= j && arr[j] > pivot)
                     j--;
-                if (i <= j)
+
+                // اگر i و j هم‌پوشانی نداشته باشند، عناصر را جابجا می‌کنیم
+                if (i < j)
                 {
-                    var temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
-                    i++;
-                    j--;
+                    Swap(arr, i, j);
                 }
             }
-            if (leftindex < j)
-                SortArray(array, leftindex, j);
-            if (i < rightIndex)
-                SortArray(array, i, rightIndex);
 
-            return array;
+            // جابجایی pivot به موقعیت صحیح
+            Swap(arr, low, j);
+            return j; // بازگشت اندیس pivot
+        }
 
+        // تابع کمکی برای جابجایی دو عنصر
+        private static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
     }
 }
